3203464d07f406673aa900e6534050514676d7c5
Sowon Kim <sso626262@naver.com>
2025-02-04 21:50:05 +0900

    feat: EMOTION constant 수정, 감정 카드 생성 (#31)

--- SoWon/react-ts/src/app/emotions/page.tsx
+++ SoWon/react-ts/src/app/emotions/page.tsx
@@ -1,3 +1,28 @@
+import { Link } from 'react-router-dom'
+import { EMOTION } from '../../constants'
+
 export default function EmotionLinkPage() {
-    return <>Emotion Link Page</>
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-col gap-3">
+                <h1 className="text-3xl text-gray-800 font-semibold">감정 상자</h1>
+                <span className="text-gray-400">나만의 감정을 돌아보고 생각에 잠겨보아요 :)</span>
+            </div>
+            <div>
+                {EMOTION.map((e) => (
+                    <Link
+                        to={`/emotions/${e.key}`}
+                        key={e.key}
+                        className="flex flex-row items-center w-full p-4 rounded-3xl border border-gray-50 hover: hover:shadow-xl"
+                    >
+                        <div className={`rounded-2xl text-6xl`}>{e.emoji}</div>
+                        <div className="flex flex-col">
+                            <h1 className="text-2xl">{e.key}</h1>
+                            <span className="text-gray-400">{e.description}</span>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
