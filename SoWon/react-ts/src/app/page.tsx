6ec5025f4f19914289ff5a8d17b1980452af68b7
Sowon Kim <sso626262@naver.com>
2025-02-04 19:55:48 +0900

    style: tailwind.css btn 클래스 이름 변경 및 적용 (#31)

--- SoWon/react-ts/src/app/page.tsx
+++ SoWon/react-ts/src/app/page.tsx
@@ -73,7 +73,7 @@ const DiaryWriter = () => {
                         <button
                             key={index}
                             type="button"
-                            className={emotion === e ? 'btn btn-emotion' : 'btn btn-invalid px-1.5 py-0.5 text-sm'}
+                            className={`btn ${emotion === e ? 'btn-green' : 'btn-gray'} px-1.5 py-0.5 text-sm`}
                             onClick={() => setEmotion(e)}
                         >
                             {e}
@@ -85,7 +85,7 @@ const DiaryWriter = () => {
                         <button
                             key={index}
                             type="button"
-                            className={weather === w ? 'btn btn-weather' : 'btn btn-invalid px-1.5 py-0.5 text-sm'}
+                            className={`btn ${weather === w ? 'btn-blue' : 'btn-gray'} px-1.5 py-0.5 text-sm`}
                             onClick={() => setWeather(w)}
                         >
                             {w}
@@ -109,7 +109,7 @@ const DiaryHistory = () => {
         <div className="w-full flex flex-col items-start gap-4 p-4 justify-between rounded-lg bg-white border border-gray-100 h-2/3 min-h-[20rem]">
             <h1 className="text-xl mt-5 text-emerald-600">기록된 일기</h1>
             <DiaryCardList diary={useDiaryValue()} />
-            <Link to="/emotions" className="btn btn-valid w-full">
+            <Link to="/emotions" className="btn btn-green w-full">
                 감정 모아보기
             </Link>
         </div>
--- SoWon/react-ts/src/components/Button.tsx
+++ SoWon/react-ts/src/components/Button.tsx
@@ -10,7 +10,7 @@ export const Button: React.FC<ButtonProps> = ({ isValid, onClick, children }) =>
     return (
         <button
             type={isValid ? 'submit' : 'button'}
-            className={isValid ? 'btn btn-valid w-full' : 'btn btn-invalid w-full'}
+            className={`${isValid ? 'btn btn-green' : 'btn btn-gray'} w-full`}
             onClick={onClick}
             disabled={!isValid}
         >
