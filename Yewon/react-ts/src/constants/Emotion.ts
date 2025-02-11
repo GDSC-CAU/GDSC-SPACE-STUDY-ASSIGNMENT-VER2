ee8301e9f066fc53a12843f94faed3a81bcaf41c
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2025-02-11 20:07:29 +0900

    feat: add Emotion constant file

---
+++ Yewon/react-ts/src/constants/Emotion.ts
@@ -0,0 +1,7 @@
+export const EmotionIcon = [
+    { key: 'bad', emoji: '🤬', color: 'red', description: '최악의 하루였어요.' },
+    { key: 'soso', emoji: '😗', color: 'purple', description: '괜찮은 하루였어요.' },
+    { key: 'good', emoji: '😙', color: 'green', description: '좋은 하루였어요.' },
+    { key: 'great', emoji: '😃', color: 'blue', description: '멋진 하루였어요.' },
+    { key: 'awesome', emoji: '😎', color: 'yellow', description: '최고의 하루였어요.' },
+]
