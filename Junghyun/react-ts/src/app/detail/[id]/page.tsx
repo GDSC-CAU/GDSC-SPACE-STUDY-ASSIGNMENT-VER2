9a5ec44b8b57bf42165d816809f8989f11f1902f
katie424 <7080katie@gmail.com>
2025-02-11 18:03:18 +0900

    feat: add delete feature

--- Junghyun/react-ts/src/app/detail/[id]/page.tsx
+++ Junghyun/react-ts/src/app/detail/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { useParams } from 'react-router-dom'
-import { useDiaryValue } from '../../../provider/Diary'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useDiaryValue, useDiaryUpdate } from '../../../provider/Diary'
+import { Diary } from '../../../interface/diary'
 
 type DiaryDetailPageParams = {
     id: string
@@ -7,13 +8,34 @@ type DiaryDetailPageParams = {
 
 export default function DiaryDetailPage() {
     const { id } = useParams<DiaryDetailPageParams>()
-    const diary = useDiaryValue().find((diary) => diary.id === id)
+    const navigate = useNavigate()
+    const diaries = useDiaryValue()
+    const diary = diaries.find((diary) => diary.id === id)
+    const updateDiaries = useDiaryUpdate()
     const formattedDate = new Date(diary!.date).toLocaleDateString('ko-KR', {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
         weekday: 'long',
     })
+
+    const handleDelete = () => {
+        if (!diary || !id) return
+
+        try {
+            updateDiaries((prev: Diary[]) => {
+                const newDiaries = prev.filter((diary) => diary.id !== id)
+                localStorage.setItem('diaries', JSON.stringify(newDiaries))
+                return newDiaries
+            })
+
+            alert('일기가 삭제되었습니다.')
+            navigate('/')
+        } catch (error) {
+            console.error('일기 삭제 실패:', error)
+            alert('일기 삭제에 실패하였습니다.')
+        }
+    }
 
     return (
         <div className="flex flex-col h-full w-1/2 justify-center gap-10">
@@ -29,7 +51,9 @@ export default function DiaryDetailPage() {
             <div className="flex h-1/2 text-xl">{diary!.content}</div>
             <div className="flex flex-row gap-2">
                 <button className="green-btn flex-1 p-2">새로운 일기 작성하기</button>
-                <button className="red-btn flex-1 p-2">현재 일기 삭제하기</button>
+                <button className="red-btn flex-1 p-2" onClick={handleDelete}>
+                    현재 일기 삭제하기
+                </button>
             </div>
         </div>
     )
