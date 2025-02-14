41bb970153469e1fe79efddb6ec38d1899b8a44f
katie424 <7080katie@gmail.com>
2025-02-14 23:00:03 +0900

    fix: fix tailwind color prob

--- Junghyun/react-ts/src/components/EmotionCard.tsx
+++ Junghyun/react-ts/src/components/EmotionCard.tsx
@@ -8,9 +8,18 @@ type EmotionProps = {
 
 export const EmojiBox = ({ emotion }: EmotionProps) => {
     const { emoji, color } = EMOTION_DATA[emotion]
+    const bgColorMap = {
+        yellow: 'bg-yellow-50 group-hover:bg-yellow-100',
+        blue: 'bg-blue-50 group-hover:bg-blue-100',
+        green: 'bg-green-50 group-hover:bg-green-100',
+        red: 'bg-red-50 group-hover:bg-red-100',
+        purple: 'bg-purple-50 group-hover:bg-purple-100',
+    }
 
     return (
-        <div className={`flex w-24 h-24 text-6xl justify-center items-center bg-${color}-50 rounded-2xl`}>{emoji}</div>
+        <div className={`flex w-24 h-24 text-6xl justify-center items-center ${bgColorMap[color]} rounded-2xl`}>
+            {emoji}
+        </div>
     )
 }
 
@@ -19,7 +28,7 @@ export default function EmotionCard({ emotion }: EmotionProps) {
 
     return (
         <Link to={`/emotions/${emotion}`}>
-            <div className="flex flex-row items-center justify-center gap-4 border border-gray-100 rounded-3xl p-5 hover:scale-105 transition-all duration-300 hover:shadow-xl hover:border-transparent cursor-pointer">
+            <div className="flex flex-row items-center justify-center gap-4 border border-gray-100 rounded-3xl p-5 hover:scale-105 transition-all duration-300 hover:shadow-xl cursor-pointer group">
                 <EmojiBox emotion={emotion} />
                 <div className="flex flex-col items-start justify-center gap-1">
                     <div className="text-2xl capitalize">{emotion}</div>
